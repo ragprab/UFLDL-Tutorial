7abd29b140ca46cb087b7b4e14e04622fdd1b51f
ragprab <rprab@umich.edu>
2016-05-09 15:43:08 -0400

    UFLDL Files
    All the UFLDL tutorial files.

---
+++ Exercise 8 - Convolutional Neural Networks/cnnPool.m
@@ -0,0 +1,44 @@
+function pooledFeatures = cnnPool(poolDim, convolvedFeatures)
+%cnnPool Pools the given convolved features
+%
+% Parameters:
+%  poolDim - dimension of pooling region
+%  convolvedFeatures - convolved features to pool (as given by cnnConvolve)
+%                      convolvedFeatures(featureNum, imageNum, imageRow, imageCol)
+%
+% Returns:
+%  pooledFeatures - matrix of pooled features in the form
+%                   pooledFeatures(featureNum, imageNum, poolRow, poolCol)
+%     
+
+numImages = size(convolvedFeatures, 2);
+numFeatures = size(convolvedFeatures, 1);
+convolvedDim = size(convolvedFeatures, 3);
+
+pooledFeatures = zeros(numFeatures, numImages, floor(convolvedDim / poolDim), floor(convolvedDim / poolDim));
+
+% -------------------- YOUR CODE HERE --------------------
+% Instructions:
+%   Now pool the convolved features in regions of poolDim x poolDim,
+%   to obtain the 
+%   numFeatures x numImages x (convolvedDim/poolDim) x (convolvedDim/poolDim) 
+%   matrix pooledFeatures, such that
+%   pooledFeatures(featureNum, imageNum, poolRow, poolCol) is the 
+%   value of the featureNum feature for the imageNum image pooled over the
+%   corresponding (poolRow, poolCol) pooling region 
+%   (see http://ufldl/wiki/index.php/Pooling )
+%   
+%   Use mean pooling here.
+% -------------------- YOUR CODE HERE --------------------
+for curFeature = 1:numFeatures
+   for curImage = 1:numImages
+      for i = 1:floor(convolvedDim / poolDim)
+         for j = 1:floor(convolvedDim / poolDim)
+            pooledFeatures(curFeature,curImage,i,j) =  mean(mean(convolvedFeatures(curFeature,curImage,1 + (i-1)*poolDim : i*poolDim, 1 + (j-1)*poolDim : j*poolDim)));
+         end
+      end
+   end
+end
+
+end
+
